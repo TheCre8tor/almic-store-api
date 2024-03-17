4e6cb859e178af41325cca0d27a93487e54aa781
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-03-17 10:32:41 +0100

    added datasource and environment variables

---
+++ database/data-source.ts
@@ -0,0 +1,30 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+// NOTE: Initialize config -->
+config();
+
+const {
+  APP_DB__HOST,
+  APP_DB__PORT,
+  APP_DB__USERNAME,
+  APP_DB__PASSWORD,
+  APP_DB__DATABASE,
+} = process.env;
+
+const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: APP_DB__HOST,
+  port: Number(APP_DB__PORT),
+  username: APP_DB__USERNAME,
+  password: APP_DB__PASSWORD,
+  database: APP_DB__DATABASE,
+  entities: [],
+  migrations: [],
+  logging: true,
+  synchronize: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export { dataSource, dataSourceOptions };
